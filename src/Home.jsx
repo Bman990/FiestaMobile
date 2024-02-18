e45c02c18c00684678edba141b0cbdc2038f0e4b
Braxton Revills <braxtonrr9@gmail.com>
2024-02-18 03:54:29 -0500

    Fixed navbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,19 @@ import Hero from './Hero';
 import Story from './Story';
 import Menu from './Menu';
 import Contact from './Contact';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const sectionId = urlParams.get('section');
+    if (sectionId) {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, []);
 
   return (
     <div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,17 +3,30 @@ import tacoLogo from '/tacoLogo.png';
 import './StyleSheet/NavStyle.css';
 import BagSvg from './bagSvg';
 import { useShoppingCart } from './ShoppingCartContext';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
 
 export default function Navbar() {
   const { cart } = useShoppingCart();
   const quantity = cart.reduce((total, item) => total + item.quantity, 0);
-
+  const location = useLocation();
 
   const handleSectionClick = (sectionId) => {
-    // Scroll to the section based on the hash in the URL after navigation
+    if (sectionId === 'cart') {
+      // If navigating to the cart, use React Router's Link
+      return;
+    }
+  
+    // For other sections, check if we are already on the home page
+    const isHomePage = location.pathname === '/';
+  
+    if (!isHomePage) {
+      // If not on the home page, navigate to the home page with the section as a hash
+      window.location.href = `/?section=${sectionId}`;
+    }
+    
+    // If on the home page, scroll to the section
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
@@ -22,9 +35,10 @@ export default function Navbar() {
 
   return (
     <nav className='navStyle'>
-      <Link to="/" onClick={() => handleSectionClick('heroSection')}>
-        <img src={tacoLogo} width='110' height='110' alt="Taco Logo" />
-      </Link>
+
+    <Link to="/" id="heroSection" onClick={() => handleSectionClick('heroSection')}>
+      <img src={tacoLogo} width='110' height='110' alt="Taco Logo" className='mobileStyleImg'/>
+    </Link>
 
       <ul className='navUL'>
         <li>
@@ -46,7 +60,7 @@ export default function Navbar() {
         </li>
 
         <li>
-          <Link to="cart" className='svgBtn'>
+          <Link to="/cart" className='svgBtn'>
             <BagSvg />
             <div className='quanityNum'>{quantity}</div>
           </Link>
